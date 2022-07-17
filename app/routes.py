4414015d63cace5b2f74aa0c6ff97189201f2021
pho <phonum5@gmail.com>
2022-07-17 23:15:33 +0000

    add edit_profile template

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@
 from datetime import datetime
 from app import app, db
 from flask import render_template, redirect, flash, url_for, request
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from flask_login import current_user, login_required, login_user, logout_user
 from app.models import User
 from werkzeug.urls import url_parse
@@ -89,3 +89,20 @@ def user(username):
     ]
 
     return render_template('user.html', user=user, posts=posts)
+
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('user', username=current_user.username))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
